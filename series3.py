5efbf77f48c2b3762de84de922d7425d4333c677
izyann <izyan.nabil@gmail.com>
2023-07-20 13:15:56 +0400

    my third program

---
+++ series3.py
@@ -0,0 +1,18 @@
+#4 3 2 1
+#4 3 2
+#4 3 
+#4
+i = 1
+j = 3 
+l = j
+while i <= j:
+    k = 1
+    c = j 
+    while k <= l:
+        print(c,end =' ')
+        k = k + 1
+        c = c - 1
+    i = i + 1
+    l = l - 1
+    print()
+    
