2dcdc9a9d3ce21945f643d65dfcbce5d0867a8b2
izyann <izyan.nabil@gmail.com>
2023-07-28 12:29:43 +0400

    my leventh program

---
+++ series11.py
@@ -0,0 +1,10 @@
+a = int(input("enter the number:"))
+b = int(input("enter the number:"))
+c = int(input("enter the number:"))
+if a > b:
+    if a > c:
+        print(a)
+    print(c)
+if b > c:
+    print(b)
+print(c)
